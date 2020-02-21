38cc697cd986767210381ee0f4b18c852baa1265
petterobam <1460300366@qq.com>
2020-02-21 21:14:41 +0800

    备份20200221

--- init/back-up.sh
+++ init/back-up.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 # 初始化备份个人Linux个人空间
-
+# 个人 home 目录
+userHomePath=$(cd `~`; pwd)
+echo "当前用户 home 目录: $userHomePath"
 # 当前文件所在目录
 basepath=$(cd `dirname $0`; pwd)
+echo "当前 shell 文件目录: $basepath"
 
 # oh-my-zsh
-cp -r ~/.oh-my-zsh/ $basepath
-cp ~/.zshrc $basepath
+echo "开始备份 oh-my-zsh ..."
+echo "拷贝 .oh-my-zsh 目录： sudo cp -r $userHomePath/.oh-my-zsh/ $basepath"
+sudo cp -r $userHomePath/.oh-my-zsh/ $basepath
+echo "拷贝 .zshrc 文件：sudo cp $userHomePath/.zshrc $basepath"
+sudo cp $userHomePath/.zshrc $basepath
+echo "备份 oh-my-zsh 结束"
 
 # vim
-cp -r ~/.vim/ $basepath
-cp ~/.vimrc $basepath
-sudo apt install vim-gtk
+echo "开始备份 vim ..."
+echo "拷贝 .vim 目录：sudo cp -r $userHomePath/.vim/ $basepath"
+sudo cp -r $userHomePath/.vim/ $basepath
+echo "拷贝 .vimrc 文件：p $userHomePath/.vimrc $basepath"
+sudo cp $userHomePath/.vimrc $basepath
+echo "备份 vim 结束"
 
 # linuxbrew 略去，用命令装
-# cp -r ~/.linuxbrew $basepath
+# echo "开始备份 Linuxbrew ..."
+# echo "拷贝 .linuxbrew 目录：sudo cp -r $userHomePath/.linuxbrew $basepath"
+# sudo cp -r $userHomePath/.linuxbrew $basepath
+# echo "在线安装 Linuxbrew：sh -c \$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+# echo "备份 Linuxbrew 结束"
 
 # java
-cp -r ~/.java $basepath
+echo "开始备份 jdk ..."
+echo "拷贝 .java 目录：sudo cp -r $userHomePath/.java $basepath"
+sudo cp -r $userHomePath/.java $basepath
+echo "备份 jdk 结束"
 
 # profile
-cp ~/.profile $basepath
+echo "开始备份 .profile ..."
+echo "拷贝 .profile 文件：sudo cp $userHomePath/.profile $basepath"
+sudo cp $userHomePath/.profile $basepath
+echo "备份 .profile 结束"
--- init/init.sh
+++ init/init.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 # 初始化个人Linux个人空间
+echo "初始化个人Linux个人空间：\n"
+echo "1、期间安装软件可能需要输入密码；\n"
+echo "2、期间安装软件请确认是否需要安装；\n"
 
-echo "初始化个人Linux个人空间："
-echo "1、期间安装软件可能需要输入密码；"
-echo "2、期间安装软件请确认是否需要安装；"
-
+# 个人 home 目录
+userHomePath=$(cd `~`; pwd)
+echo "当前用户 home 目录: $userHomePath"
 # 当前文件所在目录
 basepath=$(cd `dirname $0`; pwd)
+echo "当前 shell 文件目录: $basepath"
 
 ## zsh
 sudo apt-get install zsh
 # oh-my-zsh
-# cp -r $basepath/.oh-my-zsh/ ~/
-# git https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+# cp -r $basepath/.oh-my-zsh/ $userHomePath/
+# git https://github.com/ohmyzsh/ohmyzsh.git $userHomePath/.oh-my-zsh
 # sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # 默认 zsh
 chsh -s /bin/zsh
-cp $basepath/.zshrc ~/
+sudo cp $basepath/.zshrc $userHomePath/
 source ~/.zshrc
 
 # vim
-cp -r $basepath/.vim/ ~/
+sudo cp -r $basepath/.vim/ $userHomePath/
 # bundle 插件检出
-git clone https://github.com/davidhalter/jedi.git ~/.vim/bundle/jedi/
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree/
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle/
-git https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe/
-git https://github.com/PythonCharmers/python-future ~/.vim/bundle/YouCompleteMe/third_party/python-future
-git https://github.com/ross/requests-futures ~/.vim/bundle/YouCompleteMe/third_party/requests-futures
-git https://github.com/Valloric/ycmd ~/.vim/bundle/YouCompleteMe/third_party/ycmd
-git
-#
-cp $basepath/.vimrc ~/
+git clone https://github.com/davidhalter/jedi.git $userHomePath/.vim/bundle/jedi/
+git clone https://github.com/scrooloose/nerdtree.git $userHomePath/.vim/bundle/nerdtree/
+git clone https://github.com/gmarik/vundle.git $userHomePath/.vim/bundle/vundle/
+git https://github.com/Valloric/YouCompleteMe.git $userHomePath/.vim/bundle/YouCompleteMe/
+git https://github.com/PythonCharmers/python-future $userHomePath/.vim/bundle/YouCompleteMe/third_party/python-future
+git https://github.com/ross/requests-futures $userHomePath/.vim/bundle/YouCompleteMe/third_party/requests-futures
+git https://github.com/Valloric/ycmd $userHomePath/.vim/bundle/YouCompleteMe/third_party/ycmd
+# vim配置覆盖
+sudo cp $basepath/.vimrc $userHomePath/
 ## 初始化 vim 的时候，解决 YouCompleteMe unavailable: unable to load Python
 sudo apt install vim-gtk
 
@@ -40,12 +42,13 @@ sudo apt install vim-gtk
 sudo apt-get install build-essential curl file git
 # 为防止 curl: (7) Failed to connect to raw.githubusercontent.com port 443: 拒绝连接
 sudo apt-get install ca-certificates -y
-sh -c "$(curl -fsSL  https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+# echo "在线安装 Linuxbrew：sh -c \$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 ## linuxbrew 环境激活，可以检查下 ~/.profile
 # echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 
 # java
-cp -r $basepath/.java ~/
+sudo cp -r $basepath/.java $userHomePath/
 # jdk 环境配置，可以检查下 ~/.profile
 # echo "JAVA_HOME=~/.java/jdk/jdk1.8.0_231"  >>~/.profile
 # echo "export PATH=\$JAVA_HOME/bin:\$PATH"  >>~/.profile
@@ -62,14 +65,20 @@ gem install jekyll bundler
 
 ## rvm
 # curl -L https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer | bash -s stable
-# cp -r $basepath/.rvm ~/
+# cp -r $basepath/.rvm $userHomePath/
 # source ~/.rvm/scripts/rvm
 
 # profile
-cp $basepath/.profile ~/
+sudo cp $basepath/.profile $userHomePath/
 
 # snap 安装
 sudo apt update
 sudo apt install snapd
 
 # github desktop 安装
+
+# node、npm 安装
+sudo apt install node
+# 网站打包应用程序工具安装 https://github.com/jiahaog/nativefier
+sudo npm install nativefier -g
+# nativefier --name "AppName"  --icon myicon.icns "http://www.petterobam.cn"
